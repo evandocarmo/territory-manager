70a6fb051c13b0113b2a0d5128e5563d8bbe6216
evandocarmo <evan_hpc@hotmail.com>
2017-06-23 16:04:53 -0300

    added fix for router on heroku

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
-app.use((req, res)=>res.sendfile(__dirname + '/src/index.html'));
 // Start the app by listening on the default
 // Heroku port
+app.get('*', function (req, res) {
+  res.sendfile('./dist/index.html'); // load our index.html file
+});
 app.listen(process.env.PORT || 8080);
