2e8e1ee1e3a7f5384c0d67e681f8aa8d1045a316
evandocarmo <evan_hpc@hotmail.com>
2017-08-01 19:54:01 -0300

    cleaned up login component and auth service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,8 +3,6 @@ import { tokenNotExpired } from 'angular2-jwt';
 import { Router } from '@angular/router';
 import { Headers, Http, Response, RequestOptions  } from '@angular/http';
 import { Observable } from 'rxjs';
-import {MaterializeModule} from "angular2-materialize";
-import {MaterializeDirective, MaterializeAction} from "angular2-materialize";
 import { environment } from '../environments/environment';
 
 declare var $ : any;
@@ -28,16 +26,10 @@ export class AuthService {
   	}
   }
 
-  public authenticate(username: string, password: string):any{
-   if(!this.redirectUrl)
-       this.redirectUrl = "home";
-   if(this.loggedIn())
-     this.router.navigate([this.redirectUrl]);
-   console.log("not logged in. Try and auth");
+  public authenticate(username: string, password: string):Observable<boolean>{
    let body: string = JSON.stringify({"username":username,"password":password});
-   this.http.post(API_URL,body, { headers: contentHeaders })
-      .subscribe(
-        response => {
+   return this.http.post(API_URL,body, { headers: contentHeaders })
+      .map(response => {
           //localStorage.setItem('id_token', response.json().id_token);
           console.log(response);
           if(response.json().token){
@@ -45,17 +37,10 @@ export class AuthService {
             let user = JSON.stringify(response.json().user);
             console.log(user);
             localStorage.setItem('user',user);
-            console.log("redirect url is " + this.redirectUrl);
-            window.location.reload(true);
-            return;
+            return true;
           }else{
-            Materialize.toast("Sorry! Your credentials are incorrect.",4000,"red white-text");
           	return false;
           }
-        },
-        error => {
-          console.log(error.text());
-          return false;
         }
       );
   }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 import { Router } from "@angular/router";
 import { AuthService } from '../auth.service';
+import {MaterializeModule} from "angular2-materialize";
+import {MaterializeDirective, MaterializeAction} from "angular2-materialize";
+
+declare var Materialize:any;
 
 @Component({
   selector: 'login',
@@ -19,6 +23,17 @@ export class LoginComponent implements OnInit {
   }
 
   login(event, username,password) {
-    this.authService.authenticate(username,password);
+    this.loading = true;
+    this.authService.authenticate(username,password).subscribe(
+      response=>{
+        if(response){
+          window.location.reload();
+        }
+        else{
+          this.loading = false;
+          Materialize.toast("Sorry. Incorrect name or password",4000,"red white-text");
+        }
+      }
+    )
   }
 }
