5bf86f8797400d413f6b477b419d8757d4741cf1
evandocarmo <evan_hpc@hotmail.com>
2017-08-01 22:34:18 -0300

    imported router into home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,7 +11,7 @@ export class HomeComponent implements OnInit {
   user = JSON.parse(localStorage.getItem('user'));
   screenName = this.user.name.charAt(0).toUpperCase() + this.user.name.slice(1);
   problem = false;
-  constructor(private userService:UserService) {}
+  constructor(private userService:UserService, private router:Router) {}
 
   ngOnInit() {
     this.userService.getUserInfo().subscribe(
