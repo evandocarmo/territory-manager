e5cae87db21043eab7803190ed8675b74a33211c
evandocarmo <evan_hpc@hotmail.com>
2017-08-07 13:00:13 -0300

    force ssl redirect

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
+app.use('/*',function(req,res){
+  if(req.headers['x-forwarded-proto']!=="https"){
+    res.redirect(301,'https://' + req.get('Host') + req.url);
+  }
+})
 app.get('*', function (req, res) {
   res.sendfile('./dist/index.html'); // load our index.html file
 });
