8a7037a48f1ba8e058922e566677a1590189b0e4
evandocarmo <evan_hpc@hotmail.com>
2017-08-07 22:54:37 -0300

    added max age to express static

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 // server.js
 const express = require('express');
 const app = express();
+const
 // Run the app by serving the static files
 // in the dist directory
 // Start the app by listening on the default
 // Heroku port
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
 //app.use(forceSSL());
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist' ,{ maxAge: 86400000 }));
 app.get('*', function (req, res) {
   res.sendfile('./dist/index.html'); // load our index.html file
 });
